39cd88f83e09aaef275ab12fe1a438fa74d024fd
nick <nicholas.stapleton97@gmail.com>
2022-06-01 00:56:42 -0700

    functional keyboard

--- src/client/components/creator/vocab.creator.tsx
+++ src/client/components/creator/vocab.creator.tsx
@@ -1,12 +1,13 @@
 
-import { FormEvent, useState } from 'react';
+import { FormEvent, useState, useRef } from 'react';
 import { ICreatorUIProps } from '../../pages/ux/creator/creator.ui';
 import { TLanguage, TPartOfSpeech, TVocabSubject } from '../../../api/entities/vocab/vocab.interface';
 import styles from './Creator.module.scss';
 import { IEntity, Vocab } from '../../../api/entities/';
 import Axios from 'axios';
 import Keyboard from 'react-simple-keyboard';
-import 'react-simple-keyboard/build/css/index.css';
+import "react-simple-keyboard/build/css/index.css";
+
 // TODO dot env file
 const HOST = 'http://localhost';
 const PORT = '3000';
@@ -26,6 +27,12 @@ const SupportedSubjects = [
     "neutral", "masculine", "feminine", "neutral_plural",  "masculine_plural", "feminine_plural"
 ]
 
+import * as KeyboardSupport from '../../../api/keyboard'
+const SupportedKeyboardLanguages = {
+    'spanish': KeyboardSupport.layout_span,
+    'punjabi': KeyboardSupport.layout_punj
+}
+
 // https://www.iana.org/assignments/media-types/media-types.xhtml#image
 const SupportedImageTypes = 'image/png, image/jpeg, image/gif';
 
@@ -37,6 +44,68 @@ function fileToURL(file:any) {
     return URL.createObjectURL(file);
 }
 
+// TEST
+// https:www.w3schools.com/howto/tryit.asp?filename=tryhow_js_draggable
+let boundCheckKeyboard: (x, y) => void;
+function dragElement(grabbable, grabHeader) {
+    if(grabbable == null || grabHeader == null) return false;
+
+    console.log('adding event listeners to', grabbable, grabHeader);
+    boundCheckKeyboard = checkBounds;
+
+    var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+    if (document.getElementById(grabHeader.id)) {
+      /* if present, the header is where you move the DIV from:*/
+      document.getElementById(grabHeader.id).onmousedown = dragMouseDown;
+    } else {
+      /* otherwise, move the DIV from anywhere inside the DIV:*/
+      grabbable.onmousedown = dragMouseDown;
+    }
+
+    return true;
+    
+    function checkBounds(leftOffset, topOffset) {
+        if(topOffset < 0) topOffset = 0;
+        //f(topOffset > grabbable.offsetHeigth * 0.90) topOffset = grabbable.offsetHeigth * 0.90;
+        if(leftOffset < 0) leftOffset = 0;
+        if(leftOffset > grabbable.offsetWidth * 0.80) leftOffset = grabbable.offsetWidth * 0.80;
+
+        grabbable.style.top = topOffset + "px";
+        grabbable.style.left = leftOffset + "px";
+    }
+
+    function dragMouseDown(e) {
+      e = e || window.event;
+      e.preventDefault();
+      // get the mouse cursor position at startup:
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      // call a function whenever the cursor moves:
+      document.onmousemove = elementDrag;
+    }
+  
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        const x = grabbable.offsetLeft - pos1;
+        const y = grabbable.offsetTop - pos2;
+        // bound check and set new position
+        checkBounds(x, y);
+    }
+  
+    function closeDragElement() {
+      /* stop moving when mouse button is released:*/
+      document.onmouseup = null;
+      document.onmousemove = null;
+    }
+}
+
 const VocabCreator = ({stateManager, set, creatorManager, setCreator}: ICreatorUIProps) => {
 
     const [imageDescription, setImageDescription] = useState<string>('');
@@ -44,6 +113,28 @@ const VocabCreator = ({stateManager, set, creatorManager, setCreator}: ICreatorU
     const [image, setImage] = useState(null);
     const [sound, setSound] = useState(null);
     const [language, setLanguage] = useState<TLanguage>('english');
+    const [showKeyboard, setShowKeyboard] = useState(false);
+    
+    
+    const draggableKeyboardMenu = useRef(null);
+    const draggableHeader = useRef(null);
+    const keyboard = useRef(null);
+    const keyboardInput = useRef(null);
+    const rootInput = useRef(null);
+    const exampleInput = useRef(null);
+    const noteInput = useRef(null);
+    const descriptionInput = useRef(null);
+    const [layoutName, setLayoutName] = useState("default"); // for shift, caps lock, etc
+
+    const handleShift = () => {
+        const newLayoutName = layoutName === "default" ? "shift" : "default";
+        setLayoutName(newLayoutName);  
+    };
+
+    const onKeyboardPress = button => {
+        console.log("Button pressed", button);
+        if (button === "{shift}" || button === "{lock}") handleShift();
+    };
 
     const submitVocabPutRequest = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -135,20 +226,61 @@ const VocabCreator = ({stateManager, set, creatorManager, setCreator}: ICreatorU
         })
     }
 
+    const processKeyboardInput = buffer => {
+        if(keyboardInput.current == null) return
+        console.log('BOOP', buffer);
+        keyboardInput.current.value = buffer;
+    }
+
+    const focusKeyboardOn = r => {
+        if(r == null) return;
+        keyboardInput.current = r;
+        keyboard.current.setInput(r.value);
+    }
+
     return (
     <div id={styles.VocabCreator}>
         <div id={styles.VocabMenu}>
             {language != 'english' &&
-            <Keyboard>
-                {`${language} virtual keyboard`}
-            </Keyboard>
-            }
+            <div id={styles.KeyboardMenu} ref={draggableKeyboardMenu} >
+                {showKeyboard &&
+                <Keyboard
+                    keyboardRef={r => {keyboard.current = r}}
+                    layout={SupportedKeyboardLanguages[language]}
+                    layoutName={layoutName}
+                    onKeyPress={onKeyboardPress}
+                    onChange={processKeyboardInput}
+                />}
+                <div style={{width: '100%', display: 'flex', flexDirection: 'row'}}>
+                    <div className={styles.UserButtonWrapper} style={{width: '50%'}}>
+                        <button onClick={(e) => {
+                            e.preventDefault();
+                            setShowKeyboard((prev) => !prev);
+                            if(!dragElement(draggableKeyboardMenu.current, draggableHeader.current)) {
+                                alert('oof!');
+                            }
+                            // TODO fix this to prevent keyboard from opening off the screen
+                            // boundCheckKeyboard(draggableKeyboardMenu.current.leftOffset, draggableKeyboardMenu.current.topOffset);
+                        }}>
+                            {`toggle ${language} keyboard`}
+                        </button>
+                    </div>
+                    
+                    <div id={styles.KeyboardMoveGrabbableArea} style={{width: '50%'}}>
+                        <div id={styles.GrabbableHeader} ref={draggableHeader}>
+                            {showKeyboard && <p>Click & Drag to move</p>}
+                        </div>
+                    </div>
+                </div>
+            </div>}
+
             <form id={styles.Form} onSubmit={async (e) => {await submitVocabPutRequest(e)}}>
                 {/* vocab language */}
                 <div>
                     <p>Language</p>
-                    <select name='Lang' onChange={(e)=> {
+                    <select name='Lang' onChange={async (e)=> {
                         e.preventDefault();
+                        keyboardInput.current = null;
                         setLanguage(e.target.value as TLanguage);
                     }}>
                         {SupportedLanguages.map((lang) =>  <option key={lang}>{lang}</option>)}
@@ -158,19 +290,28 @@ const VocabCreator = ({stateManager, set, creatorManager, setCreator}: ICreatorU
                 {/* root vocab value */}
                 <div>
                     <p>Root Value</p>
-                    <input name='Root' placeholder='Root'/>
+                    <input ref={rootInput} name='Root' placeholder='Root' onFocus={(e) => {
+                        e.preventDefault();
+                        focusKeyboardOn(rootInput.current);
+                    }}/>
                 </div>
 
                 {/* the word in english */}
                 <div>
                     <p>Translation</p>
-                    <input name='Trans' placeholder='Translation'/>
+                    <input name='Trans' placeholder='Translation' onFocus={(e)=>{
+                        e.preventDefault();
+                        keyboardInput.current = null;
+                    }}/>
                 </div>
 
-                {/* the word in english */}
+                {/* example phrase */}
                 <div>
                     <p>Example Phrase</p>
-                    <input name='Example' placeholder='Example Usage'/>
+                    <input ref={exampleInput} name='Example' placeholder='Example Usage' onFocus={(e) => {
+                        e.preventDefault();
+                        focusKeyboardOn(exampleInput.current);
+                    }}/>
                 </div>
 
                 {/* part of speach */}
@@ -191,7 +332,10 @@ const VocabCreator = ({stateManager, set, creatorManager, setCreator}: ICreatorU
 
                 {/* notes on the vocab item */}
                 <div>
-                    <textarea placeholder='vocab note' onChange={(e)=>{setVocabNote(e.target.value)}}></textarea>
+                    <textarea ref={noteInput} placeholder='vocab note' onFocus={(e)=>{
+                        e.preventDefault();
+                        focusKeyboardOn(noteInput.current);
+                    }} onChange={(e)=>{setVocabNote(e.target.value)}}></textarea>
                 </div>
 
                 {/* image uploading */}
@@ -211,8 +355,8 @@ const VocabCreator = ({stateManager, set, creatorManager, setCreator}: ICreatorU
                 <div>
                     <input type='Submit' placeholder='Create Vocab'/>
                 </div>
-
             </form>
+
         </div>
 
         <div id={styles.VocabMedia}>
@@ -220,7 +364,10 @@ const VocabCreator = ({stateManager, set, creatorManager, setCreator}: ICreatorU
             {image &&
             <div id={styles.ImageWrapper}>
                 <img src={fileToURL(image)}></img>
-                <textarea placeholder='Image Description' onChange={(e)=>{setImageDescription(e.target.value)}}></textarea>
+                <textarea ref={descriptionInput} placeholder='Image Description' onFocus={(e)=>{
+                    e.preventDefault();
+                    focusKeyboardOn(descriptionInput.current);
+                }} onChange={(e)=>{setImageDescription(e.target.value)}}></textarea>
             </div>}
 
             {/* previews the sound */}
